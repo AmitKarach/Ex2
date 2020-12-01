e74d71cb683585bf7052e58b577edcaad4427beb
AmitKarach <karach.amit30@gmail.com>
2020-12-01 16:33:33 +0200

    the new files from nadav

--- src/api/edge_data.java
+++ src/api/edge_data.java
@@ -1,5 +1,5 @@
 package api;
-/**
+/**c
  * This interface represents the set of operations applicable on a 
  * directional edge(src,dest) in a (directional) weighted graph.
  * @author boaz.benmoshe
