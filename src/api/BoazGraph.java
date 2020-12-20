1fc7c73c304f57494215e7636f86b41fef945ca9
AmitKarach <karach.amit30@gmail.com>
2020-12-20 21:18:31 +0200

    changed everything

--- src/api/BoazGraph.java
+++ src/api/BoazGraph.java
@@ -4,6 +4,9 @@ import com.google.gson.annotations.SerializedName;
 
 import java.util.List;
 
+/**
+ * this class is here to help us change the node from his json form to our loved node
+ */
 public class BoazGraph
 {
     @SerializedName(value = "Edges")
@@ -11,7 +14,9 @@ public class BoazGraph
     @SerializedName(value = "Nodes")
     List<BoazNode> nodes;
 
-
+    /**
+     * the node that we use here
+     */
     static class BoazNode {
         int id;
         String pos;
